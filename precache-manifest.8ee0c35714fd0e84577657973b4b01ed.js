ebbd2e02b21382359c73ad7439c4979956639cd4
yuvraj jaiswal <yuvrajjaiswal655@gmail.com>
2022-09-24 12:10:36 +0530

    first commit

---
+++ precache-manifest.8ee0c35714fd0e84577657973b4b01ed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6076a8ed5faae0023733",
+    "url": "/static/css/main.eeb1a1cb.chunk.css"
+  },
+  {
+    "revision": "6076a8ed5faae0023733",
+    "url": "/static/js/main.6076a8ed.chunk.js"
+  },
+  {
+    "revision": "cf6b6bf545d5975ec723",
+    "url": "/static/js/1.cf6b6bf5.chunk.js"
+  },
+  {
+    "revision": "6c83903425a4a0582c3c",
+    "url": "/static/js/2.6c839034.chunk.js"
+  },
+  {
+    "revision": "2a4bae67bc577351aa24",
+    "url": "/static/js/runtime~main.2a4bae67.js"
+  },
+  {
+    "revision": "5104f01786274b42d3b18cfee152eff7",
+    "url": "/index.html"
+  }
+];
